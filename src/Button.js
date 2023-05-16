62caa47feef7be89b80791d6ccfd19677e676a5d
Danik616 <ddanielvargas2011@gmail.com>
2023-05-16 16:55:37 -0500

    Se añade un evento en linea

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types"
 
 export function Button ({text, name= "Juan"}){
-    return <button>
+    return <button onClick={function () {
+        console.log("Hola mundo")
+    }}>
         {text} -{name}
     </button>
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { TaskCard } from "./Task"
+import { Button } from "./Button"
 const rootElement=document.getElementById("root")
 const root= ReactDOM.createRoot(rootElement)
 
 root.render(<div>
     <TaskCard ready={false}/>
+    <Button/>
 </div>)
 
