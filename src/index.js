9e3c953167228c54d0b02ae7397ec9892192636b
Danik616 <ddanielvargas2011@gmail.com>
2023-05-16 13:16:00 -0500

    Se crea mensaje de prueba por componentes

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from "react-dom/client"
 const rootElement=document.getElementById("root")
 const root= ReactDOM.createRoot(rootElement)
 
-root.render(<h1> Hello World!!</h1>)
+function Greeting(){
+    return <h1> Este es un componente</h1>
+}
+
+
+root.render(Greeting())
