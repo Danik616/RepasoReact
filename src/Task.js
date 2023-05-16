c3af9d8d768c957b172accde434cc8c471b7b434
Danik616 <ddanielvargas2011@gmail.com>
2023-05-16 16:07:04 -0500

    Se crean los estilos por medio del archivo css

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
-//Para nosotros poder agregar estilos, nosotros tenemos que escribirlo en la propiedad style pero dentro de dos llaves para que no haya problema con React, la llave de adentro es un objeto tipo JSON donde van a entrar estos cambios de las propiedades
+import "./tasks.css"
 export function TaskCard(){
     return (
-        <div style={{background: "#202020", color: "#fff"}}>
+        <div class="card">
             <h1>Mi primer tarea</h1>
             <p>Tarea realizada</p>
         </div>
