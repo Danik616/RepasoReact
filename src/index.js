81169f94b0ffce83728f8094136b44a393866e5d
Danik616 <ddanielvargas2011@gmail.com>
2023-05-16 14:58:41 -0500

    Se soluciona problema de greeting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Greeting from "./Greeting"
+import {Greeting} from "./Greeting"
 const rootElement=document.getElementById("root")
 const root= ReactDOM.createRoot(rootElement)
 
