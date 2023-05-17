5ad6fdb4fb9d7f5e66a664f4037331d24456cba5
Danik616 <ddanielvargas2011@gmail.com>
2023-05-17 08:13:26 -0500

    Se crea un form para subir eventos

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import { Button } from "./Button"
 const rootElement=document.getElementById("root")
 const root= ReactDOM.createRoot(rootElement)
 
+
+/*Se utiliza el evento onSubmit para poder enviar la informacion de un formulario por
+medio de un botón, usamos la propiedad e.preventDefault para poder evitar que la página se
+refresque y que envie por consola la información */
 root.render(<div>
     <TaskCard ready={false}/>
     <Button text="Saludar"/>
 
-    <input onChange={function(e){
-        console.log(e.target.value)
-    }}></input>
+    <form onSubmit={(e) => {
+        e.preventDefault()
+        console.log("Se ha enviado correctamente")
+    }}>
+        <h1>Formulario de registro</h1>
+        <button>Send</button>
+    </form>
 </div>)
 
