e41f84d25d30a75b83a91807dfc7debd59b5745d
Danik616 <ddanielvargas2011@gmail.com>
2023-05-17 08:47:04 -0500

    Se hace manejo de los arreglos en React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { TaskCard } from "./Task"
-import { Button } from "./Button"
-import { Post } from "./Posts"
-const rootElement=document.getElementById("root")
-const root= ReactDOM.createRoot(rootElement)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { TaskCard } from "./Task";
+import { Button } from "./Button";
+import { Post } from "./Posts";
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
+const users = [
+  {
+    id: 1,
+    name: "John",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 1,
+    name: "John",
+    image: "https://robohash.org/user2",
+  },
+];
 
-root.render(<div>
-    <Post/>
-</div>)
-
+root.render(
+  <div>
+    {users.map((user, index) => {
+      return <div key={index}>
+        <h1 >{user.name}</h1>
+        <img src={user.image}/>
+      </div>
+    })}
+  </div>
+);
