f64f7d990709646041b7c447e63845476b04221d
Danik616 <ddanielvargas2011@gmail.com>
2023-05-17 08:31:12 -0500

    Se traen blibliotecas de iconos

--- src/Posts.js
+++ src/Posts.js
@@ -1,3 +1,7 @@
+// Se hace la instalacion de iconos en react. npm install react-icons
+
+//En este caso se esta usando la libreria de visual studio code=vsc, pero hay mas.
+import { VscBug } from "react-icons/vsc";
 export const Post = () => {
     return <button onClick={() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -5,6 +9,7 @@ export const Post = () => {
         .then(data => console.log(data))
 
     }}>
+        <VscBug />
         Traer datos
     </button>
 }
